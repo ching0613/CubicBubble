ea5524a22d1eea4b981ffb74334f4819a6c87d64
CharlesYSH <yanghsiang7@gmail.com>
2018-03-28 01:41:24 +0800

    GameActivityTest

---
+++ app/src/main/java/com/example/wen_cheng/cubicbubble/DrawGame.java
@@ -0,0 +1,20 @@
+package com.example.wen_cheng.cubicbubble;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by YANG on 2018/3/28.
+ */
+
+public class DrawGame extends View {
+    public DrawGame(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
